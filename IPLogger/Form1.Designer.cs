fac5159cfa5fe7294751b53557551e809fb0cda7
Akulav <43496076+Akulav@users.noreply.github.com>
2021-02-16 22:14:40 +0200

    Initial Release

---
+++ IPLogger/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Petru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ip = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ip
+            // 
+            this.ip.AutoSize = true;
+            this.ip.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ip.Location = new System.Drawing.Point(12, 9);
+            this.ip.Name = "ip";
+            this.ip.Size = new System.Drawing.Size(63, 51);
+            this.ip.TabIndex = 0;
+            this.ip.Text = "IP";
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(21, 165);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(363, 69);
+            this.start.TabIndex = 1;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 273);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.ip);
+            this.Name = "Form1";
+            this.Text = "IP DETECTOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ip;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ IPLogger/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Petru
+{
+    public partial class Form1 : Form
+    {
+        public string buffer = GetIPAddress();
+        private Timer timer1;
+        public Form1()
+        {
+            InitializeComponent();
+            string root = @"C:\IP";
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+
+            TextWriter tsw = new StreamWriter(@"C:\IP\IP.txt", true);
+            tsw.WriteLine(buffer + " - " + DateTime.Now.ToString("T") + "\n");
+            tsw.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            InitTimer();
+        }
+
+        static string GetIPAddress()
+        {
+            String address = "";
+            try
+            {
+                WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+                using (WebResponse response = request.GetResponse())
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    address = stream.ReadToEnd();
+                }
+
+                int first = address.IndexOf("Address: ") + 9;
+                int last = address.LastIndexOf("</body>");
+                address = address.Substring(first, last - first);
+
+                return address;
+            }
+
+            catch { return "ERROR"; }
+        }
+
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 5000; // in miliseconds
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string data = GetIPAddress();
+            
+            if (data == buffer) { ip.Text = data; goto END; }
+
+            if (data == "ERROR") { goto END; }
+            ip.Text = data;
+            buffer = data;
+
+            TextWriter tsw = new StreamWriter(@"C:\IP\IP.txt", true);
+            tsw.WriteLine(ip.Text + " - " + DateTime.Now.ToString("T") + "\n");
+            tsw.Close();
+        END:;
+        }
+    }
+}
